6f93226122b0b1b8ce211f2dd855c69b27bedf87
Dhruvraj1821 <dhruvraj18211821@gmail.com>
2026-03-31 16:04:23 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { Queue } from "bullmq";
+import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+    }
+});
+
+const redisConnection = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+});
+
+const jobQueue = new Queue("jobQueue", {
+    connection: redisConnection,
+});
+
+app.get("/", (req, res) => {
+    res.send("Backpressure Server Running");
+});
+
+io.on("connection", (socket) => {
+    console.log("Client connected: ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected:", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+
+httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
